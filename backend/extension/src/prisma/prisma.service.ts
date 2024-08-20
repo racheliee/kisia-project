52f215de1a55f3836d08d543ae7578fc58835ae2
racheliee <rachelie.dwg@gmail.com>
2024-08-20 10:40:33 +0900

    feat(be): create prisma module and remove app.controller and app.service

---
+++ backend/extension/src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    private readonly logger = new Logger(PrismaService.name);
+
+    async onModuleInit() {
+        try{
+            await this.$connect();
+            this.logger.log('Connected to database');
+        } catch (error){
+            this.logger.error("Error connecting to database", error);
+        }
+    }
+
+    async onModuleDestroy() {
+        try{
+            await this.$disconnect();
+            this.logger.log('Disconnected from database');
+        } catch (error){
+            this.logger.error("Error disconnecting from database", error);
+        }
+    }
+}
