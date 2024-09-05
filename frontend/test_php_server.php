c282f405099335ac541d1a23ed8be14cb832ae5a
hy30nq <121498814+hy30nq@users.noreply.github.com>
2024-09-06 00:48:40 +0900

    feat: add random sleep delay between 1-8 seconds

--- frontend/test_php_server.php
+++ frontend/test_php_server.php
@@ -1,24 +1,20 @@
 <?php
 
-header('Access-Control-Allow-Origin: *'); // 특정 도메인에서의 접근만 허용
-header('Access-Control-Allow-Headers: Content-Type'); // Content-Type 헤더 허용
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS'); // POST, GET, OPTIONS 메소드 허용
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Content-Type: application/json');
 
-sleep(2);
-// JSON 형식의 입력을 읽기
-$data = json_decode(file_get_contents('php://input'), true);
+sleep(rand(1, 8));
 
-$url = $data['url'];
-// $url = 'https://naver.com'
+$data = json_decode(file_get_contents('php://input'), true);
 
 $response = [
     'statusCode' => 200,
     'message' => 'Successfully received and processed URL',
     'data' => [
-        'url' => $url,
-        'isMalicious' => false,
-        'source' => 0
+        'url' => $data['url'],
+        'isMalicious' => false
     ]
 ];
 
