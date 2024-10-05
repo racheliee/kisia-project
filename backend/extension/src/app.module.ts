50a00a027aaace50d57cf565ead7824323fd4ad8
racheliee <rachelie.dwg@gmail.com>
2024-10-05 00:35:16 -0700

    feat(be): implement admin model apis

--- backend/extension/src/app.module.ts
+++ backend/extension/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UrlModule } from './url/url.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [PrismaModule, UrlModule],
+  imports: [PrismaModule, UrlModule, AdminModule],
   controllers: [],
   providers: [],
 })
