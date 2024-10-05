bf2952722a276233b78c8da91687d9391d490c6c
racheliee <rachelie.dwg@gmail.com>
2024-10-05 02:42:45 -0700

    fix(be): fix prisma migration in docker-entrypoint

--- backend/extension/docker-entrypoint.sh
+++ backend/extension/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 dockerize -wait tcp://0.0.0.0:5432 -timeout 60s
 
 echo "Running migrations"
-npx prisma migrate dev --name init
+# npx prisma migrate dev --name init
+npx prisma migrate deploy # run all migrations
 
 # echo "Seeding database"
 
