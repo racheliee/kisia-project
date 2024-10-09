ca9e50d4d8364e991205bf0f780dba80018e8955
racheliee <rachelie.dwg@gmail.com>
2024-10-08 21:43:56 -0700

    refactor(be): prettier format

--- backend/extension/src/auth/jwt.strategy.ts
+++ backend/extension/src/auth/jwt.strategy.ts
@@ -1,31 +1,35 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { PassportStrategy } from "@nestjs/passport";
-import { Request } from "express";
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UserService } from "src/user/user.service";
-import { JwtPayload } from "./auth.service";
-import { User } from "@prisma/client";
+import { UserService } from 'src/user/user.service';
+import { JwtPayload } from './auth.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    constructor(private readonly userService: UserService, 
-        private readonly configService: ConfigService,
-    ) {
-        super({
-            jwtFromRequest: ExtractJwt.fromExtractors([
-                (request) => {
-                    return request?.cookies?.access_token;
-                },
-            ]),
-            ignoreExpiration: false,
-            secretOrKey: configService.get<string>('JWT_REFRESH_SECRET'),
-            passReqToCallback: true,
-        });
-    }
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request) => {
+          return request?.cookies?.access_token;
+        },
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_REFRESH_SECRET'),
+      passReqToCallback: true,
+    });
+  }
 
-    async validate(req: Request, payload: JwtPayload): Promise<User> {
-        const refreshToken = req.cookies['refresh_token'];
-        return this.userService.getUserIfRefreshTokenMatches(refreshToken, payload.id);
-    }
+  async validate(req: Request, payload: JwtPayload): Promise<User> {
+    const refreshToken = req.cookies['refresh_token'];
+    return this.userService.getUserIfRefreshTokenMatches(
+      refreshToken,
+      payload.id,
+    );
+  }
 }
