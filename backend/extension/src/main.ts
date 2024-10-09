9a6ecc82b6b8cf1ee0a7848ad153e486c063ba2d
racheliee <rachelie.dwg@gmail.com>
2024-10-08 22:49:14 -0700

    feat(be): add url history per request

--- backend/extension/src/main.ts
+++ backend/extension/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,7 +10,9 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+  });
 
   app.use(cookieParser());
 
