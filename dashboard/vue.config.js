31e3f7574a96d08788bdf8fe0653d7c4cd310ba5
hy30nq <121498814+hy30nq@users.noreply.github.com>
2024-10-10 14:52:36 +0900

    fix(fe): Update vue.config.js

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 여기에 devServer 설정을 추가하면 됩니다.
+  devServer: {
+    proxy: {
+      '*': {
+        target: 'http://43.203.239.57:8000',
+        changeOrigin: true,
+        pathRewrite: { '^/': '/' },
+      },
+    },
+  },
 })
