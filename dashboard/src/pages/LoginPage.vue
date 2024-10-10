e4fe53809d7a100c958e067a7154ab055c8fcd92
hy30nq <121498814+hy30nq@users.noreply.github.com>
2024-10-10 12:30:00 +0900

    bug(fe): change https method

--- dashboard/src/pages/LoginPage.vue
+++ dashboard/src/pages/LoginPage.vue
@@ -64,7 +64,7 @@ export default {
             password: this.password,
           },
           {
-            headers: { "Content-Type": "application/json" },
+            headers: { "Content-Type": "application/json" }
           }
         );
 
