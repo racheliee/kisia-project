c9392a62bccb4b9b20b756e9fbf275ff854ec401
hy30nq <121498814+hy30nq@users.noreply.github.com>
2024-10-10 10:51:30 +0900

    feat(fe): Update vue.config.js

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -1,4 +1,19 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/': {
+        target: 'http://43.203.239.57:8000',  // 백엔드 서버 URL
+        secure: false,  // HTTPS가 아닌 HTTP 요청을 허용
+        changeOrigin: true  // 요청의 origin 헤더를 백엔드 서버로 설정
+      }
+    }
+  }
 })
