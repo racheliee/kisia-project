7a7f097e28c6074a36a21f78b2e993ed42736b6f
racheliee <rachelie.dwg@gmail.com>
2024-10-12 02:52:45 -0700

    feat(be): implemented top related apis and db overview api

--- backend/extension/src/admin/admin.controller.ts
+++ backend/extension/src/admin/admin.controller.ts
@@ -1,4 +1,121 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Query,
+} from '@nestjs/common';
+import { AdminModule } from './admin.module';
+import { AdminService } from './admin.service';
+import { TotalRequestsQueryDTO } from './dto/totalrequest.dto';
+import { UrlDto } from 'src/url/dto/url.dto';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  private readonly logger = new Logger(AdminModule.name);
+  constructor(private readonly adminService: AdminService) {}
+
+  @Get('db-overview')
+  async getDbOverview() {
+    try {
+      const result = await this.adminService.getDbOverview();
+      return {
+        statusCode: 200,
+        message: 'Successfully retrieved database overview',
+        data: {
+          size: result.size,
+          totalUrls: result.totalUrls,
+          numMaliciousUrls: result.numMaliciousUrls,
+        },
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException('Error retrieving database overview');
+    }
+  }
+
+  @Get('total-requests')
+  async getTotalRequests(@Query() query: TotalRequestsQueryDTO) {
+    try {
+      return {
+        statusCode: 200,
+        message: 'Successfully retrieved total requests',
+        data: await this.adminService.getTotalRequests(query.interval),
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException('Error retrieving total requests');
+    }
+  }
+
+  @Get('top-urls')
+  async getTop5AccessedUrls() {
+    try {
+      return {
+        statusCode: 200,
+        message: 'Successfully retrieved top 5 accessed urls',
+        data: await this.adminService.getTop5AccessedUrls(),
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException('Error retrieving top 5 accessed urls');
+    }
+  }
+
+  @Get('top-malicious')
+  async getTop5MaliciousUrls() {
+    try {
+      return {
+        statusCode: 200,
+        message: 'Successfully retrieved top 5 malicious urls',
+        data: await this.adminService.getTop5MaliciousUrls(),
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException('Error retrieving top 5 malicious urls');
+    }
+  }
+
+  @Get('top-false-negative')
+  async getTop5FalseNeg() {
+    try {
+      return {
+        statusCode: 200,
+        message: 'Successfully retrieved top 5 false negative urls',
+        data: await this.adminService.getTop5FalseNeg(),
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException('Error retrieving top 5 malicious urls');
+    }
+  }
+
+  @Get('top-false-positive')
+  async getTop5FalsePos() {
+    try {
+      return {
+        statusCode: 200,
+        message: 'Successfully retrieved top 5 false positive urls',
+        data: await this.adminService.getTop5FalsePos(),
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException('Error retrieving top 5 malicious urls');
+    }
+  }
+
+  @Get('url-search')
+  async searchUrl(@Body() url: UrlDto){
+    try {
+        return {
+            statusCode: 200,
+            message: 'Successfully retrieved url information',
+            data: await this.adminService.searchUrl(url.url),
+        }
+    } catch (error) {
+        this.logger.error(error);
+        throw new BadRequestException('Error retrieving url information');
+    }
+  }
+}
---
+++ backend/extension/src/admin/admin.service.ts
@@ -0,0 +1,171 @@
+import { Injectable, Logger, Query } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { TotalRequestsQueryDTO } from './dto/totalrequest.dto';
+
+@Injectable()
+export class AdminService {
+  private readonly logger = new Logger(AdminService.name);
+  constructor(private readonly prismaService: PrismaService) {}
+
+  // database overview
+  async getDbOverview() {
+    this.logger.log('Getting database overview');
+
+    const size = await this.prismaService
+      .$queryRaw`SELECT pg_size_pretty(pg_database_size(current_database())) as size;`;
+    const totalUrls = await this.prismaService.url.count();
+    const numMaliciousUrls = await this.prismaService.url.count({
+      where: { isMalicious: true },
+    });
+
+    return {
+      size: size[0].size,
+      totalUrls,
+      numMaliciousUrls,
+    };
+  }
+
+  // todo: total requests
+  async getTotalRequests(interval: string) {
+    switch (interval) {
+      case 'day':
+        return this.prismaService.history.groupBy({
+          by: ['createdAt'],
+          orderBy: {
+            createdAt: 'asc',
+          },
+          where: {
+            createdAt: {
+              gte: new Date(new Date().getTime() - 24 * 60 * 60 * 1000), // 24 hours ago
+            },
+          },
+          _count: {
+            id: true,
+          },
+        });
+      case 'week':
+        return this.prismaService.history.groupBy({
+          by: ['createdAt'],
+          orderBy: {
+            createdAt: 'asc',
+          },
+          _count: {
+            id: true,
+          },
+        });
+    }
+  }
+
+  async getTop5AccessedUrls() {
+    const res = await this.prismaService.url.findMany({
+      take: 5,
+      orderBy: {
+        accessCount: 'desc',
+      },
+      where: {
+        // recent 30 days
+        updatedAt: {
+          gte: new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000),
+        },
+      },
+    });
+
+    // map as url: accessCount
+    return res.map((url) => {
+      return {
+        url: url.url,
+        accessCount: url.accessCount,
+        isMalicious: url.isMalicious,
+      };
+    });
+  }
+
+  async getTop5MaliciousUrls() {
+    const res = await this.prismaService.url.findMany({
+      take: 5,
+      where: {
+        isMalicious: true,
+        updatedAt: {
+          gte: new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000),
+        },
+      },
+      orderBy: {
+        accessCount: 'desc',
+      },
+    });
+
+    return res.map((url) => {
+      return {
+        url: url.url,
+        accessCount: url.accessCount,
+      };
+    });
+  }
+
+  async getTop5FalseNeg() {
+    const res = await this.prismaService.url.findMany({
+      take: 5,
+      orderBy: {
+        falseNeg: 'desc',
+      },
+      where: {
+        // recent 30 days
+        updatedAt: {
+          gte: new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000),
+        },
+      },
+    });
+
+    return res.map((url) => {
+      return {
+        url: url.url,
+        falseNegCount: url.falseNeg,
+      };
+    });
+  }
+
+  async getTop5FalsePos() {
+    const res = await this.prismaService.url.findMany({
+      take: 5,
+      orderBy: {
+        falsePos: 'desc',
+      },
+      where: {
+        // recent 30 days
+        updatedAt: {
+          gte: new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000),
+        },
+      },
+    });
+
+    return res.map((url) => {
+      return {
+        url: url.url,
+        falsePosCount: url.falsePos,
+      };
+    });
+  }
+
+  async searchUrl(url: string) {
+    const res = await this.prismaService.url.findFirst({
+        where:{
+            url: url,
+        }
+    });
+
+    if(!res){
+        throw new Error('URL not found');
+    }
+
+    return {
+        url: url,
+        isMalicious: res.isMalicious,
+        accessCount: res.accessCount,
+        falseNeg: res.falseNeg,
+        falsePos: res.falsePos,
+        confidenceScore: res.confidenceScore,
+        createdAt: res.createdAt,
+        updatedAt: res.updatedAt,
+    };
+  }
+}
